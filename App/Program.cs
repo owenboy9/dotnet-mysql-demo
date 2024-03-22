3e84cee92dcfcef997d7e99eb9d8d9fc9b516915
owenboy9 <owenkraus9@gmail.com>
2024-03-22 14:11:17 +0100

    get data from db

--- App/Program.cs
+++ App/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MySql.Data.MySqlClient;
+
+MySqlConnection db = null;
+
+string connectionString = "server=localhost;uid=root;pwd=mypassword;database=seeSharp;port=3306";
+
+try
+{
+    db = new MySqlConnection(connectionString);
+    db.Open();
+
+    string query = "SELECT * FROM users";
+    MySqlCommand command = new(query, db);
+
+    using MySqlDataReader reader = command.ExecuteReader();
+    
+    while(reader.Read())
+    {
+        int id = reader.GetInt32("id");
+        string username = reader.GetString("username");
+        Console.WriteLine($"{username} has the id: {id}");
+    }
+    
+}
+catch(MySqlException e)
+{
+    Console.WriteLine(e);
+}
+finally
+{
+    db?.Close();
+}
